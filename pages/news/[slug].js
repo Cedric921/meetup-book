2a9351776fe8437ab2362acc15bf757a34d69ae9
cedric karungu <ckarungu921@gmail.com>
2022-09-08 04:34:12 +0200

    feat: add dynamic routes

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,9 +1,19 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const SingleNewPage = () => {
 	const router = useRouter();
-	console.log(router.query.slug);
-	return <div>SingleNewPage</div>;
+	// for getting params in url
+	const slug = router.query.slug;
+	return (
+		<>
+			<h3>
+				<Link href='/news'>All News</Link>
+			</h3>
+			<div>SingleNewPage</div>
+			<p>{slug}</p>
+		</>
+	);
 };
 
 export default SingleNewPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 
 const newsPage = () => {
-	return <div>newsPage</div>;
+	return (
+		<>
+			<div>newsPage</div>
+			<ul>
+				<li>
+					<Link href='/news/next-js-awesome'>Next js is awesome</Link>
+				</li>
+				<li>
+					<Link href='/news/react-powefull'>React is powefull for web</Link>
+				</li>
+				<li>
+					<Link href='/news/js'>Javascript all in one</Link>
+				</li>
+			</ul>
+		</>
+	);
 };
 
 export default newsPage;
-   
