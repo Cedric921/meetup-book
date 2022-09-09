1bdc514212c8418423f4f0ae3692692a33d93806
cedric karungu <ckarungu921@gmail.com>
2022-09-09 11:30:42 +0200

    feat: add POST add meetup route and connection

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import NewMeetupForm from '../../components/meetups/newMeetupForm';
 
 const NewMeetupPage = () => {
-	const addMeetupHanler = (newMeetup) => {
-		console.log(newMeetup);
+	const addMeetupHanler = async (newMeetup) => {
+		await fetch('/api/new-meetup', {
+			method: 'POST',
+			body: JSON.stringify(newMeetup),
+			headers: { 'Content-Type': 'application/json' },
+		});
+
+		const data = await response.json();
+		console.log(data);
 	};
 	return <NewMeetupForm onAdd={addMeetupHanler} />;
 };
